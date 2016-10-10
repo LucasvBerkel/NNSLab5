d2e5e88d589f7d766087ad519bc09e6584f92b00
Lucas <lucasvberkel@gmail.com>
2016-10-10 16:43:00 +0200

    Deel af

--- pipesensor.py
+++ pipesensor.py
@@ -37,19 +37,25 @@ def getNeighbours(peer, mcast_addr):
     peer.sendto(enc_message, mcast_addr)
         
 
-def initiateEcho(peer, neighbours, sequenceNumber, operation, sensor_val=0, kill=False):
+def initiateEcho(peer, neighbours, sequenceNumber, operation, sensor_val=0):
     payload = 0
     if operation == 3 or operation == 4:
         payload = sensor.val
-    if neighbours == []:
-        kill = True
+    if neighbours == [] and operation == 1:
+        address = peer.getsockname()
+        message = message_encode(3, sequenceNumber, sensor.pos, (0,0), operation, sensor_val, 0)
+        peer.sendto(message, address)
+    elif neighbours == []:
+        address = peer.getsockname()
+        message = message_encode(3, sequenceNumber, sensor.pos, (0,0), operation, sensor_val, sensor_val)
+        peer.sendto(message, address)
     else:
         for neighbour in neighbours:
             message = message_encode(2, sequenceNumber, sensor.pos, (0,0),
                                      operation, sensor_val, payload)
             peer.sendto(message, neighbour[0])
     sequenceNumber += 1
-    return sequenceNumber, kill
+    return sequenceNumber
 
 
 def forward_echo(peer, neighbours, init_seq, init_pos, operation, father_addr, payload, sensor_val=0):
@@ -92,23 +98,6 @@ def send_echo_reply(peer, init_seq, init_pos, father_addr, operation, payload, i
         peer.sendto(message, father_addr)
 
 
-def readInput():
-    string = ""
-    while True:
-        # Read a single character.
-        char = sys.stdin.read(1)
-
-        # If the character is a newline, we have our data!
-        if char == "\n":
-            break
-
-        # Append the character to the string.
-        string += char
-        # Flush stdout to make sure other processes can read from it.
-    sys.stdout.flush()
-    return string
-
-
 def main(mcast_addr,
     sensor_pos, sensor_range, sensor_val,
     grid_size, ping_period):
@@ -144,110 +133,111 @@ def main(mcast_addr,
         peer.bind(('', INADDR_ANY))
 
     getNeighbours(peer, mcast_addr)
-    time.sleep(2)
     neighbours = []
     timeCounter = 0
     sequenceNumber = 0
     echo_log = {}
     operation_list = ["echo", "size", "sum", "min", "max", "same"]
     # -- This is the event loop. --
+    # commandList = ["size", "sum", "max"]
+    commandList = ["size", "min", "max", ""]
+    indexCommand = 0
     counter = 0
+    print(sensor.pos)
+    while True:
+        [rlist, wlist, xlist] = select.select([mcast, peer], [], [], 0)
+        # add neighbors to list
+        for neighbor_socket in rlist:
+            data, address = neighbor_socket.recvfrom(512)
+            dec_message = message_decode(data)
 
-    for command in ['size', 'sum', 'min']:
-        kill = False
-        while not kill:
-            print("counter " + str(counter))
-            counter += 1
-            [rlist, wlist, xlist] = select.select([mcast, peer], [], [], 0)
+            messType = dec_message[0]
+            sequence = dec_message[1]
+            initPos = dec_message[2]
+            neighPos = dec_message[3]
+            operation = dec_message[4]
+            capability = dec_message[5]
+            payload = dec_message[6]
+            if messType != 0 and messType != 1:
+                print(dec_message)
+            if messType == 0:
+                pos_init = initPos
+                distance = getDistance(pos_init, sensor.pos)
+                if distance == 0:
+                    continue
+                elif distance < capability:
+                    enc_message = message_encode(1, 0, pos_init, sensor.pos)
+                    peer.sendto(enc_message, address)
+            elif messType == 1:
+                neighbours.append([address, neighPos])
+            elif messType == 2:
+                keyLog = str(sequence) + str(initPos)
 
-            # add neighbors to list
-            for neighbor_socket in rlist:
-                data, address = neighbor_socket.recvfrom(512)
-                dec_message = message_decode(data)
-
-                messType = dec_message[0]
-                sequence = dec_message[1]
-                initPos = dec_message[2]
-                neighPos = dec_message[3]
-                operation = dec_message[4]
-                capability = dec_message[5]
-                payload = dec_message[6]
-
-                if messType == 0:
-                    pos_init = initPos
-                    distance = getDistance(pos_init, sensor.pos)
-                    if distance == 0:
-                        continue
-                    elif distance < capability:
-                        enc_message = message_encode(1, 0, pos_init, sensor.pos)
-                        peer.sendto(enc_message, address)
-                elif messType == 1:
-                    neighbours.append([address, neighPos])
-                elif messType == 2:
-                    keyLog = str(sequence) + str(initPos)
-
-                    if echo_log.get(keyLog) != None:
-                        send_echo_reply(peer, sequence, initPos, address, operation, payload, 0, capability)
-                    elif len(neighbours) == 1:
-                        send_echo_reply(peer, sequence, initPos, address, operation, payload, 1, capability)
+                if echo_log.get(keyLog) != None:
+                    send_echo_reply(peer, sequence, initPos, address, operation, payload, 0, capability)
+                elif len(neighbours) == 1:
+                    send_echo_reply(peer, sequence, initPos, address, operation, payload, 1, capability)
+                else:
+                    echo_log[keyLog] = []
+                    echo_log[keyLog].append(len(neighbours) - 1)
+                    opcode = operation
+                    if opcode == 3 or opcode == 4:
+                        echo_log[keyLog].append(sensor.val)
                     else:
-                        echo_log[keyLog] = []
-                        echo_log[keyLog].append(len(neighbours) - 1)
-                        opcode = operation
-                        if opcode == 3 or opcode == 4:
-                            echo_log[keyLog].append(sensor.val)
-                        else:
-                            echo_log[keyLog].append(0)
-                            echo_log[keyLog].append(address)
-                            forward_echo(peer, neighbours, sequence, initPos, operation,
-                                         address, payload, capability)
-                elif messType == 3:
-                    messLog = echo_log[str(sequence) + str(initPos)]
-                    if messLog[0] == 1:
-                        if sequence <= sequenceNumber and initPos == sensor.pos:
+                        echo_log[keyLog].append(0)
+                        echo_log[keyLog].append(address)
+                        forward_echo(peer, neighbours, sequence, initPos, operation,
+                                     address, payload, capability)
+            elif messType == 3:
+                messLog = echo_log[str(sequence) + str(initPos)]
+                if messLog[0] == 1 or messLog[0] == 0:
+                    if sequence <= sequenceNumber and initPos == sensor.pos:
 
-                            if operation == 1:
-                                print("networksize " + str(payload + messLog[1] + 1) + "\n")
-                                kill = True
-                            elif operation == 2:
-                                print("valuesum " + str(payload + messLog[1] + sensor.val) + "\n")
-                                kill = True
-                            elif operation == 3:
-                                if messLog[1] > payload:
-                                    messLog[1] = payload
-                                print("minimumval " + str(messLog[1]) + "\n")
-                                kill = True
-                            elif operation == 4:
-                                if messLog[1] < payload:
-                                    messLog[1] = payload
-                                print("maximumval" + str(messLog[1]) + "\n")
-                                kill = True
-                            elif operation == 5:
-                                print("sameval " + str(payload + messLog[1] + 1) + "\n")
-                                kill = True
-                        else:
-                            father_addr = messLog[2]
-                            op_list = [1, 2, 5]
-                            if operation in op_list:
-                                send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1] + payload, 2, capability)
-                            elif operation == 3:
-                                if messLog[1] > payload:
-                                    messLog[1] = payload
-                                    send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
-                            elif operation == 4:
-                                if messLog[1] < payload:
-                                    messLog[1] = payload
-                                    send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
-                                    del messLog
+                        if operation == 1:
+                            print("networksize " + str(payload + messLog[1] + 1) + "\n")
+                            indexCommand += 1
+                        elif operation == 2:
+                            print("valuesum " + str(payload + messLog[1] + sensor.val) + "\n")
+                            indexCommand += 1
+                        elif operation == 3:
+                            if messLog[1] > payload:
+                                messLog[1] = payload
+                            print("minimumval " + str(messLog[1]) + "\n")
+                            indexCommand += 1
+                        elif operation == 4:
+                            if messLog[1] < payload:
+                                messLog[1] = payload
+                            print("maximumval " + str(messLog[1]) + "\n")
+                            indexCommand += 1
+                        elif operation == 5:
+                            print("sameval " + str(payload + messLog[1] + 1) + "\n")
+                            indexCommand += 1
                     else:
-                        messLog[0] -= 1
-                        if operation == 1 or operation == 2 or operation == 5:
-                            messLog[1] += payload
-                        elif operation == 3 and messLog[1] > payload:
-                            messLog[1] = payload
-                        elif operation == 4 and messLog[1] < payload:
-                            messLog[1] = payload
+                        father_addr = messLog[2]
+                        op_list = [1, 2, 5]
+                        if operation in op_list:
+                            send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1] + payload, 2, capability)
+                        elif operation == 3:
+                            if messLog[1] > payload:
+                                messLog[1] = payload
+                                send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
+                        elif operation == 4:
+                            if messLog[1] < payload:
+                                messLog[1] = payload
+                                send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
+                    del messLog
+                else:
+                    messLog[0] -= 1
+                    if operation == 1 or operation == 2 or operation == 5:
+                        messLog[1] += payload
+                    elif operation == 3 and messLog[1] > payload:
+                        messLog[1] = payload
+                    elif operation == 4 and messLog[1] < payload:
+                        messLog[1] = payload
+        if (counter % 300) == 0 and timeCounter > 0:
+            command = commandList[indexCommand]
             if command in operation_list:
+                print("Command sent " + command)
                 opcode = operation_list.index(command)
                 echo_log[str(sequenceNumber) + str(sensor.pos)] = []
                 echo_log[str(sequenceNumber) + str(sensor.pos)].append(len(neighbours))
@@ -255,14 +245,15 @@ def main(mcast_addr,
                     echo_log[str(sequenceNumber) + str(sensor.pos)].append(sensor.val)
                 else:
                     echo_log[str(sequenceNumber) + str(sensor.pos)].append(0)
-                    sequenceNumber, kill  = initiateEcho(peer, neighbours, sequenceNumber, opcode, sensor.val)
-                    time.sleep(0.1)
-                    timeCounter += 1
-                command = ""
-            if timeCounter == ping_period:
-                neighbours = []
-                getNeighbours(peer, mcast_addr)
-                timeCounter = 0
+                sequenceNumber = initiateEcho(peer, neighbours, sequenceNumber, opcode, sensor.val)
+        time.sleep(0.01)
+        timeCounter += 1
+        counter += 1
+        if timeCounter == ping_period:
+            neighbours = []
+            getNeighbours(peer, mcast_addr)
+            timeCounter = 0
+
                 
 
 
@@ -275,7 +266,7 @@ pos = random_position(100)
 value = randint(0, 100)
 mcast_addr = ('224.1.1.1', 50100)
 sensor = sensor(pos, value, args.range)
-period = 5
+period = 200
 grid = 100
 main(mcast_addr, pos, args.range, value, grid, period)
 
