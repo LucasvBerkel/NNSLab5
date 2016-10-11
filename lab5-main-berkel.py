114bfa25cc769d6a0471faa9af7aa8813aa93458
Lucas <lucasvberkel@gmail.com>
2016-10-11 15:07:54 +0200

    Hello

--- lab5-main-berkel.py
+++ lab5-main-berkel.py
@@ -1,6 +1,6 @@
-## Netwerken en Systeembeveiliging Lab 5 - Distributed Sensor Network
-## NAME: Julian Main, Lucas van Berkel
-## STUDENT ID: ... , 10747958
+# Netwerken en Systeembeveiliging Lab 5 - Distributed Sensor Network
+# NAME: Julian Main, Lucas van Berkel
+# STUDENT ID: ... , 10747958
 import sys
 import struct
 import select
@@ -20,8 +20,8 @@ def random_position(n):
 
 
 def main(mcast_addr,
-    sensor_pos, sensor_range, sensor_val,
-    grid_size, ping_period):
+         sensor_pos, sensor_range, sensor_val,
+         grid_size, ping_period):
     """
     mcast_addr: udp multicast (ip, port) tuple.
     sensor_pos: (x,y) sensor position tuple.
@@ -82,7 +82,7 @@ def main(mcast_addr,
             operation = dec_message[4]
             capability = dec_message[5]
             payload = dec_message[6]
-            
+
             if messType == 0:
                 pos_init = initPos
                 distance = getDistance(pos_init, sensor.pos)
@@ -96,10 +96,12 @@ def main(mcast_addr,
             elif messType == 2:
                 keyLog = str(sequence) + str(initPos)
                 window.writeln(str(dec_message))
-                if echo_log.get(keyLog) != None:
-                    send_echo_reply(peer, sequence, initPos, address, operation, payload, 0, capability)
+                if echo_log.get(keyLog) is not None:
+                    send_echo_reply(peer, sequence, initPos,
+                                    address, operation, payload, 0, capability)
                 elif len(neighbours) == 1:
-                    send_echo_reply(peer, sequence, initPos, address, operation, payload, 1, capability)
+                    send_echo_reply(peer, sequence, initPos, address,
+                                    operation, payload, 1, capability)
                 else:
                     echo_log[keyLog] = []
                     echo_log[keyLog].append(len(neighbours) - 1)
@@ -109,43 +111,61 @@ def main(mcast_addr,
                     else:
                         echo_log[keyLog].append(0)
                     echo_log[keyLog].append(address)
-                    forward_echo(peer, neighbours, sequence, initPos, operation,
+                    forward_echo(peer, neighbours, sequence,
+                                 initPos, operation,
                                  address, payload, capability)
             elif messType == 3:
                 window.writeln(str(dec_message))
                 messLog = echo_log[str(sequence) + str(initPos)]
                 if messLog[0] == 1:
                     if sequence <= sequenceNumber and initPos == sensor.pos:
-                        window.writeln("Echo wave " + str(sequence) + " is decided.")
+                        window.writeln("Echo wave " +
+                                       str(sequence) + " is decided.")
                         if operation == 0:
                             window.writeln("Payload " + str(payload))
                         elif operation == 1:
-                            window.writeln("Size network is " + str(payload + messLog[1] + 1))
+                            window.writeln("Size network is " +
+                                           str(payload + messLog[1] + 1))
                         elif operation == 2:
-                            window.writeln("The sum of values is " + str(payload + messLog[1] + sensor.val))
+                            window.writeln("The sum of values is " +
+                                           str(payload + messLog[1] +
+                                               sensor.val))
                         elif operation == 3:
                             if messLog[1] > payload:
                                 messLog[1] = payload
-                            window.writeln("The minimum value of the netwerk is " + str(messLog[1]))
+                            window.writeln("The mini\
+                                            mum value of the netwerk is " +
+                                           str(messLog[1]))
                         elif operation == 4:
                             if messLog[1] < payload:
                                 messLog[1] = payload
-                            window.writeln("The maximum value of the netwerk is " + str(messLog[1]))
+                            window.writeln("The maximum value of\
+                                            the netwerk is " +
+                                           str(messLog[1]))
                         elif operation == 5:
-                            window.writeln("Numbers of sensors with same value is " + str(payload + messLog[1] + 1))
+                            window.writeln("Numbers of sensors with\
+                                            same value is " +
+                                           str(payload + messLog[1] + 1))
                     else:
                         father_addr = messLog[2]
                         op_list = [1, 2, 5]
                         if operation in op_list:
-                            send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1] + payload, 2, capability)
+                            send_echo_reply(peer, sequence, initPos,
+                                            father_addr, operation,
+                                            messLog[1] + payload,
+                                            2, capability)
                         elif operation == 3:
                             if messLog[1] > payload:
                                 messLog[1] = payload
-                            send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
+                            send_echo_reply(peer, sequence, initPos,
+                                            father_addr, operation, messLog[1],
+                                            2, capability)
                         elif operation == 4:
                             if messLog[1] < payload:
                                 messLog[1] = payload
-                            send_echo_reply(peer, sequence, initPos, father_addr, operation, messLog[1], 2, capability)
+                            send_echo_reply(peer, sequence, initPos,
+                                            father_addr, operation, messLog[1],
+                                            2, capability)
                     del messLog
                 else:
                     messLog[0] -= 1
@@ -155,7 +175,6 @@ def main(mcast_addr,
                         messLog[1] = payload
                     elif operation == 4 and messLog[1] < payload:
                         messLog[1] = payload
-                        
 
         line = window.getline()
         subParts = line.split(" ")
@@ -177,21 +196,28 @@ def main(mcast_addr,
                     window.writeln("Port: " + str(neighbour[0][1]))
                     window.writeln("")
         elif subParts[0] == "set":
-            if int(subParts[1]) >= 20 and int(subParts[1]) <= 70 and (int(subParts[1]) % 10) == 0:
+            if int(subParts[1]) >= 20 and int(subParts[1]) <= 70
+            and (int(subParts[1]) % 10) == 0:
                 sensor.srange = int(subParts[1])
                 neighbours = []
                 getNeighbours(peer, mcast_addr)
             else:
-                window.writeln("Range need to be between 20 and 70(with steps of 10)") 
+                window.writeln("Range need to be between\
+                                20 and 70(with steps of 10)")
         elif subParts[0] in operation_list:
             opcode = operation_list.index(subParts[0])
             echo_log[str(sequenceNumber) + str(sensor.pos)] = []
-            echo_log[str(sequenceNumber) + str(sensor.pos)].append(len(neighbours))
+            echo_log[str(sequenceNumber) +
+                     str(sensor.pos)].append(len(neighbours))
             if opcode == 3 or opcode == 4:
-                echo_log[str(sequenceNumber) + str(sensor.pos)].append(sensor.val)
+                echo_log[str(sequenceNumber) +
+                         str(sensor.pos)].append(sensor.val)
             else:
-                echo_log[str(sequenceNumber) + str(sensor.pos)].append(0)
-            sequenceNumber = initiateEcho(peer, neighbours, sequenceNumber, window, opcode, sensor.val)
+                echo_log[str(sequenceNumber) +
+                         str(sensor.pos)].append(0)
+            sequenceNumber = initiateEcho(peer, neighbours,
+                                          sequenceNumber, window,
+                                          opcode, sensor.val)
         time.sleep(0.1)
         timeCounter += 1
         if timeCounter == ping_period:
@@ -200,18 +226,18 @@ def main(mcast_addr,
             timeCounter = 0
 
 
-
-
 def getDistance(pos1, pos2):
     return np.sqrt(np.power(pos1[0] - pos2[0], 2) + np.power(pos1[1] - pos2[1],
                                                              2))
 
+
 def getNeighbours(peer, mcast_addr):
     enc_message = message_encode(0, 0, sensor.pos, (0, 0), 0, sensor.srange, 0)
     peer.sendto(enc_message, mcast_addr)
-        
 
-def initiateEcho(peer, neighbours, sequenceNumber, window, operation, sensor_val=0):
+
+def initiateEcho(peer, neighbours, sequenceNumber,
+                 window, operation, sensor_val=0):
     payload = 0
     if operation == 3 or operation == 4:
         payload = sensor.val
@@ -219,21 +245,24 @@ def initiateEcho(peer, neighbours, sequenceNumber, window, operation, sensor_val
         window.writeln("You have no neighbours to send the echo to")
     else:
         for neighbour in neighbours:
-            message = message_encode(2, sequenceNumber, sensor.pos, (0,0),
+            message = message_encode(2, sequenceNumber, sensor.pos, (0, 0),
                                      operation, sensor_val, payload)
             peer.sendto(message, neighbour[0])
         sequenceNumber += 1
     return sequenceNumber
 
 
-def forward_echo(peer, neighbours, init_seq, init_pos, operation, father_addr, payload, sensor_val=0):
+def forward_echo(peer, neighbours, init_seq, init_pos,
+                 operation, father_addr, payload, sensor_val=0):
     for neighbour in neighbours:
         if neighbour[0] != father_addr:
-            message = message_encode(2, init_seq, init_pos, (0, 0), operation, sensor_val, payload)
+            message = message_encode(2, init_seq, init_pos, (0, 0),
+                                     operation, sensor_val, payload)
             peer.sendto(message, neighbour[0])
 
 
-def send_echo_reply(peer, init_seq, init_pos, father_addr, operation, payload, ident, sensor_val=0):
+def send_echo_reply(peer, init_seq, init_pos, father_addr,
+                    operation, payload, ident, sensor_val=0):
     if operation == 0:
         message = message_encode(3, init_seq, init_pos, (0, 0), 0, 0, 0)
         peer.sendto(message, father_addr)
@@ -242,27 +271,32 @@ def send_echo_reply(peer, init_seq, init_pos, father_addr, operation, payload, i
             payload = ident
         else:
             payload += 1
-        message = message_encode(3, init_seq, init_pos, (0,0), operation, 0, payload)
-        peer.sendto(message, father_addr)        
+        message = message_encode(3, init_seq, init_pos,
+                                 (0, 0), operation, 0, payload)
+        peer.sendto(message, father_addr)
     elif operation == 2:
         if ident > 0:
             payload += sensor.val
-        message = message_encode(3, init_seq, init_pos, (0,0), operation, 0, payload)
+        message = message_encode(3, init_seq, init_pos,
+                                 (0, 0), operation, 0, payload)
         peer.sendto(message, father_addr)
     elif operation == 3:
         if ident > 0 and payload > sensor.val:
             payload = sensor.val
-        message = message_encode(3, init_seq, init_pos, (0,0), operation, 0, payload)
+        message = message_encode(3, init_seq, init_pos,
+                                 (0, 0), operation, 0, payload)
         peer.sendto(message, father_addr)
     elif operation == 4:
         if ident > 0 and payload < sensor.val:
             payload = sensor.val
-        message = message_encode(3, init_seq, init_pos, (0,0), operation, 0, payload)
+        message = message_encode(3, init_seq, init_pos,
+                                 (0, 0), operation, 0, payload)
         peer.sendto(message, father_addr)
     elif operation == 5:
         if sensor.val == sensor_val and ident > 0:
             payload += 1
-        message = message_encode(3, init_seq, init_pos, (0,0), operation, sensor_val, payload)
+        message = message_encode(3, init_seq, init_pos,
+                                 (0, 0), operation, sensor_val, payload)
         peer.sendto(message, father_addr)
 
 
@@ -275,7 +309,7 @@ class sensor:
         self.pos = pos
         self.val = val
         self.srange = srange
-    
+
 # -- program entry point --
 if __name__ == '__main__':
     import argparse
